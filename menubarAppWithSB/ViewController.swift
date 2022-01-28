255c6f827b8cf2d8fa9e9f487d5e7add58acc5e4
Murat Can Koç <muratcankoc@gmail.com>
2022-01-28 22:53:46 +0300

    Initial Commit

---
+++ menubarAppWithSB/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  menubarAppWithSB
+//
+//  Created by Murat Can KOÇ on 28.01.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
